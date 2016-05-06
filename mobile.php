2faf9e04d046244280b065034955c3e0fcf72d89
pechgvohd <leo93114@yahoo.com.tw>
2016-05-06 21:15:23 +0800

    Added files via upload

---
+++ mobile.php
@@ -0,0 +1,94 @@
+<html>
+<head>
+<link rel="shortcut icon" href="image/favicon.ico">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>次是慶宇免費服務</title><body><link rel="shortcut icon" href="image/favicon.ico" />
+<style>
+*:link, *:visited, *:hover, *:active, *:focus , * {
+-o-transition: color .20s linear, background-color .20s linear, border-color .20s linear;
+-webkit-transition: color .20s linear, background-color .20s linear, border-color .20s linear;
+-moz-transition: color .20s linear, background-color .20s linear, border-color .20s linear;
+transition: color .20s linear, background-color .20s linear, border-color .20s linear;
+}
+a:link {color:#9900ff;}
+a:visited {color:#9900ff;}
+a:hover {color:#ff0099;}
+::selection {
+background: #9900ff;
+color: #fff;
+}
+::-moz-selection {
+background: #9900ff;
+color: #fff;
+}
+img::selection {
+background: transparent;
+}
+img::-moz-selection {
+background: transparent;
+}
+#Img3{
+    opacity: 1.0; //設為完全不透明
+    filter: alpha(opacity=100); // IE8 與更早的版本
+}
+#Img3:hover{
+    opacity: 0.5; //透明度設為 0.5
+    filter: alpha(opacity=50); // IE8 與更早的版本
+}
+.myButton {
+	background-color:#d9d2e9;
+	-moz-border-radius:10px;
+	-webkit-border-radius:10px;
+	border-radius:10px;
+	border:1px solid #9900ff;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial, Tahoma, Helvetica, FreeSans, Microsoft JhengHei, Heiti TC, TW-Kai, sans-serif;
+	font-size:19px;
+	padding:12px 37px;
+	text-decoration:none;
+}
+.myButton:hover {
+	background-color:#fbafe3;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+img{
+ max-width:100%;
+ height:auto;
+}
+</style>
+<span style="font-family:Arial, Tahoma, Helvetica, FreeSans, Microsoft JhengHei, Heiti TC, TW-Kai, sans-serif;">
+<a href="http://jochgvofd.gq" class="myButton"><img src="image/Back.gif" width="20" height="20"> 返回官網</a><br>
+<div class="separator" style="clear: both; text-align: center;">
+<img src="image/logo.png"></a></div><br />
+<center><h1>服務一覽</h1></center>
+<br><center><a href="http://img.jochost.tk" class="myButton">次是慶宇圖片上傳</a></center><br>
+<center><a href="http://joch.gq" class="myButton">次是慶宇縮短網址</a></center><br>
+<center><a href="http://jochost.tk" class="myButton">jochost.tk免費主機</a><br><br><a href="http://jochost.gq" class="myButton">jochost.gq免費主機</a></center><br>
+<center><a href="http://free.joch.gq/ip" class="myButton">次是慶宇IP查詢</a></center>
+<br />
+</a>
+<center><a href="http://free.joch.gq/email" class="myButton">次是慶宇電子信箱貼紙產生器</a></center>
+<br />
+<center><a href="http://free.joch.gq/qr" class="myButton">次是慶宇QR Code產生器</a></center><br>
+<center><a href="http://free.joch.gq/colorcobe" class="myButton">次是慶宇色碼表</a><br><br></center>
+<center><a href="http://free.joch.gq/qq" class="myButton">次是慶宇迅雷快車網址轉換</a></center>
+<center><br><a href="html" class="myButton">次是慶宇HTML教學網</a><br><br><a href="http://ts.joch.gq" class="myButton">次是慶宇免費流量計數器 <span style="color:#ffffff;"><span style="background-color:#ff0099;">NEW</span></a></center>
+<center>
+<font size=5 color="#9900ff">
+<marquee direction="up" behavior="slide" bgcolor="#8e7cc3">
+<div style="text-align: center;"><script src="http://ts.joch.gq/cf.php?username=leo93114"></script><br />
+聯絡我們<br /><a href="mailto:jochgvofd@gmail.com"> <img src="image/taddyshen.png" /><br /><a href="https://line.me/ti/p/%40trr7250q"><img height="36" border="0" alt="好友人數" src="http://biz.line.naver.jp/line_business/img/btn/addfriends_zh-Hant.png"></a><br /><a href="https://www.facebook.com/newjochgvofd">Facebook粉絲團<a><br />
+© 2012-2015 <a href="http://jochgvofd.gq/" target="_blank">次是慶宇工作室</a> - © 2012-2015 <a href="http://wwww.wartw.tk/" target="_blank">風雨人多元工作室</a>合作<br />
+</font>
+</marquee>
+<a href="pc.php" class="myButton">電腦版網頁</a></div>
+</span>
+</head>
+</body>
+</html>
---
+++ pc.php
@@ -0,0 +1,170 @@
+<html>
+<head>
+<link rel="shortcut icon" href="image/favicon.ico">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>次是慶宇免費服務</title><body><link rel="shortcut icon" href="image/favicon.ico" />
+<style>
+*:link, *:visited, *:hover, *:active, *:focus , * {
+-o-transition: color .20s linear, background-color .20s linear, border-color .20s linear;
+-webkit-transition: color .20s linear, background-color .20s linear, border-color .20s linear;
+-moz-transition: color .20s linear, background-color .20s linear, border-color .20s linear;
+transition: color .20s linear, background-color .20s linear, border-color .20s linear;
+}
+a:link {color:#9900ff;}
+a:visited {color:#9900ff;}
+a:hover {color:#ff0099;}
+::selection {
+background: #9900ff;
+color: #fff;
+}
+::-moz-selection {
+background: #9900ff;
+color: #fff;
+}
+img::selection {
+background: transparent;
+}
+img::-moz-selection {
+background: transparent;
+}
+#Img3{
+    opacity: 1.0; //設為完全不透明
+    filter: alpha(opacity=100); // IE8 與更早的版本
+}
+#Img3:hover{
+    opacity: 0.5; //透明度設為 0.5
+    filter: alpha(opacity=50); // IE8 與更早的版本
+}
+.myButton {
+	background-color:#d9d2e9;
+	-moz-border-radius:10px;
+	-webkit-border-radius:10px;
+	border-radius:10px;
+	border:1px solid #9900ff;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial, Tahoma, Helvetica, FreeSans, Microsoft JhengHei, Heiti TC, TW-Kai, sans-serif;
+	font-size:19px;
+	padding:12px 37px;
+	text-decoration:none;
+}
+.myButton:hover {
+	background-color:#fbafe3;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+.Buttonbig {
+	background-color:#d9d2e9;
+	-moz-border-radius:13px;
+	-webkit-border-radius:13px;
+	border-radius:13px;
+	border:1px solid #9900ff;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial, Tahoma, Helvetica, FreeSans, Microsoft JhengHei, Heiti TC, TW-Kai, sans-serif;
+	font-size:40px;
+	padding:22px 76px;
+	text-decoration:none;
+}
+.Buttonbig:hover {
+	background-color:#fbafe3;
+}
+.Buttonbig:active {
+	position:relative;
+	top:1px;
+}
+</style>
+<span style="font-family:Arial, Tahoma, Helvetica, FreeSans, Microsoft JhengHei, Heiti TC, TW-Kai, sans-serif;">
+<font size=5 color="#fff">
+<marquee onMouseOver="this.stop()" onMouseOut="this.start()" bgcolor="#9900ff">歡迎使用次是慶宇免費服務，請建議使用最新瀏覽器，網頁如果空白請多次重整頁面。</marquee></font>
+<a href="http://jochgvofd.gq" class="myButton"><img src="image/Back.gif" width="20" height="20"> 返回官網</a>
+<div class="separator" style="clear: both; text-align: center;">
+<img src="image/logo.png"></a></div><br />
+<a href="http://img.jochost.tk" class="myButton">次是慶宇圖片上傳</a>
+<font size=5 color="#fff">
+<marquee direction="up" behavior="slide" bgcolor="#9900ff">
+次是慶宇免費圖片上傳，可支援JPG PNG BMP GIF格式圖片。
+</marquee>
+</font>
+<br />
+</a>
+<a href="http://joch.gq" class="myButton">次是慶宇縮短網址</a>
+<font size=5 color="#fff">
+<marquee direction="up" behavior="slide" bgcolor="#9900ff">
+次是慶宇縮短網址，快速產生。
+</marquee>
+</font>
+<br />
+<a href="http://jochost.tk" class="myButton">jochost.tk免費主機</a><a href="http://jochost.gq" class="myButton">jochost.gq免費主機</a>
+<font size=5 color="#fff">
+<marquee direction="up" behavior="slide" bgcolor="#9900ff">
+次是慶宇給大家提供免費虛擬主機，
+10GB大容量、100GB超大流量、支援php、MySQL。
+</marquee>
+</font>
+<br />
+<a href="http://free.joch.gq/ip" class="myButton">次是慶宇IP查詢</a>
+<font size=5 color="#fff">
+<marquee direction="up" behavior="slide" bgcolor="#9900ff">
+次是慶宇IP查詢，線上查看IP地址。
+</marquee>
+</font>
+<br />
+</a>
+<a href="http://free.joch.gq/email" class="myButton">次是慶宇電子信箱貼紙產生器</a>
+<font size=5 color="#fff">
+<marquee direction="up" behavior="slide" bgcolor="#9900ff">
+次是慶宇電子信箱貼紙產生器，快速產生。
+</marquee>
+</font>
+<br />
+</a>
+<a href="http://free.joch.gq/qr" class="myButton">次是慶宇QR Code產生器</a>
+<font size=5 color="#fff">
+<marquee direction="up" behavior="slide" bgcolor="#9900ff">
+次是慶宇QR Code產生器，快速產生可掃描。
+</marquee>
+<a href="http://free.joch.gq/colorcobe" class="myButton">次是慶宇色碼表</a>
+</font>
+<font size=5 color="#fff">
+<marquee direction="up" behavior="slide" bgcolor="#9900ff">
+次是慶宇色碼表，查看色碼專用。
+</marquee>
+<a href="http://free.joch.gq/qq" class="myButton">次是慶宇迅雷快車網址轉換</a>
+</font>
+<font size=5 color="#fff">
+<marquee direction="up" behavior="slide" bgcolor="#9900ff">
+次是慶宇迅雷快車網址轉換，快速線上轉換迅雷、快車、旋風qq下載網址。
+</marquee>
+</font>
+<a href="html" class="myButton">次是慶宇HTML教學網</a>
+</font>
+<font size=5 color="#fff">
+<marquee direction="up" behavior="slide" bgcolor="#9900ff">
+次是慶宇HTML教學網，線上看HTML學習製作網頁。
+</marquee>
+</font>
+<a href="http://ts.joch.gq" class="myButton">次是慶宇免費流量計數器 <span style="color:#ffffff;"><span style="background-color:#ff0099;">NEW</span></a>
+</font>
+<font size=5 color="#fff">
+<marquee direction="up" behavior="slide" bgcolor="#9900ff">
+次是慶宇免費流量計數器，查看你的網站流量。
+</marquee>
+</font>
+<font size=5 color="#9900ff">
+<marquee direction="up" behavior="slide" bgcolor="#8e7cc3">
+<div style="text-align: center;"><script src="http://ts.joch.gq/cf.php?username=leo93114"></script><br />
+聯絡我們<br /><a href="mailto:jochgvofd@gmail.com"> <img src="image/taddyshen.png" /><br /><a href="https://line.me/ti/p/%40trr7250q"><img height="36" border="0" alt="好友人數" src="http://biz.line.naver.jp/line_business/img/btn/addfriends_zh-Hant.png"></a><br /><a href="https://www.facebook.com/newjochgvofd">Facebook粉絲團<a><br />
+© 2012-2015 <a href="http://jochgvofd.gq/" target="_blank">次是慶宇工作室</a> - © 2012-2015 <a href="http://wwww.wartw.tk/" target="_blank">風雨人多元工作室</a>合作</div>
+</font>
+</marquee>
+<center>
+<a href="mobile.php" class="Buttonbig">回到手機版網頁</a></center>
+</span>
+</head>
+</body>
+</html>
